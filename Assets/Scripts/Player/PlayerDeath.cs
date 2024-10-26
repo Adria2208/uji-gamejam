65338b4ab7571006dcb1dcfecdebf0443d6648e3
Carles1-ASIX <carlesrocherm@gmail.com>
2024-10-26 18:33:32 +0200

    cambios

---
+++ Assets/Scripts/Player/PlayerDeath.cs
@@ -0,0 +1,65 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Runtime.InteropServices;
+using UnityEngine;
+
+public class PlayerDeath : MonoBehaviour
+{
+    public int lives;
+    public bool canHarm = true;
+    private Rigidbody2D rb;
+    public int time;
+    private Vector2 velocity;
+
+    //Knockback
+    public PlayerMovement force;
+    public float knockBackForce = 10;
+    public float knockBackForceUp = 2;
+    public float KBCounter;
+    public float KBTotalTime;
+
+    public bool knockFromRight;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision) {
+
+        if (collision.gameObject.layer == LayerMask.NameToLayer("Enemy"))
+        {
+
+        
+            if (lives > 0){
+                if (canHarm){
+                    harm();
+                }
+            }
+            else
+            {
+                Die();
+            }
+        }
+    }
+
+
+    private void harm()
+    {
+        canHarm = false;
+        lives--;
+        rb.AddForce (Vector2.up*knockBackForceUp, ForceMode2D.Impulse);
+        StartCoroutine(DeathHandler());
+    }
+    private void Die()
+    {
+        Destroy (gameObject);
+    }
+    IEnumerator DeathHandler()
+    {
+        
+        yield return new WaitForSeconds(time);
+        canHarm = true;
+    }
+}
+
