517edffa36e479c426ce191a2d955f56b6b5d132
Mar Castellà Vidal <al448057@uji.es>
2024-10-26 21:17:00 +0200

    Añadido objeto recogible: caramelo
    Codigos con dependencias en Mario "player" y gameManager "candyManager"

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -5,9 +5,23 @@ public class Player : MonoBehaviour
 {
     [SerializeField] private Rigidbody2D rigidbody;
 
+    public CarameloManager cm;
+
+
     private void Start()
     {
         rigidbody = GetComponent<Rigidbody2D>();
+
+        cm = GameObject.Find("GameManager").GetComponent<CarameloManager>();
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+     if (other.CompareTag("Candy"))  
+        {
+            Destroy(other.gameObject);
+            cm.carameloCount++;
+        }
     }
 
 }
