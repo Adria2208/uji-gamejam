9de7bdee8fb18a9165ecf050d405c797a18a3d53
Carles1-ASIX <carlesrocherm@gmail.com>
2024-10-26 23:22:57 +0200

    Boton de iniciar y finalizar

--- Assets/Scripts/Manager/GameManager.cs
+++ Assets/Scripts/Manager/GameManager.cs
@@ -1,4 +1,5 @@
 using UnityEngine;
+using TMPro;
 
 public class GameManager : MonoBehaviour
 {
@@ -6,14 +7,17 @@ public class GameManager : MonoBehaviour
     public static GameManager Instance { get; private set; }
 
     [SerializeField] private HouseHealth[] houses;
-
+    public GameObject listaCasas;
     public int playerLives = 5;
-
+    public TextMeshProUGUI EndText;
+    public GameObject victroyText;
+    public GameObject looseText;
     private void Awake()
     {
         if (Instance != null && Instance != this)
         {
             Destroy(this);
+            
         }
         else
         {
@@ -23,6 +27,9 @@ public class GameManager : MonoBehaviour
 
     private void Start() {
         UIManager.Instance.UpdateLivesCounter(playerLives);
+        victroyText.SetActive(false);
+        looseText.SetActive(false);
+        Time.timeScale = 1;
     }
 
     // public void HouseInteracted(House house){
@@ -37,6 +44,33 @@ public class GameManager : MonoBehaviour
 
     public int GetCurrentLives(){
         return playerLives;
+    }
+    void Update()
+    {
+        
+        victory();
+        Defeat();
+    }
+
+    //Acabar el juego
+        //Completar todas las casas
+    public void victory()
+    {
+      if (listaCasas.GetComponent<HouseList>().completed == true)
+        {
+            victroyText.SetActive(true);
+            Time.timeScale = 0;
+        }  
+    }
+        //Morir
+    public void Defeat()
+    {
+        if (playerLives == 0)
+        {
+        
+            looseText.SetActive(true);
+            Time.timeScale = 0;
+        }       
     }
 
 }
